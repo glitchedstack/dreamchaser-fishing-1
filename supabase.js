d1afda3c0549cdde20105ca7e5cfa9acc964d6e4
glitchedstack <jaydimauro6@gmail.com>
2026-07-16 19:41:32 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,4 @@
-supabase.js.txt
-const SUPABASE_URL = "https://trlytqrdjmvbihntvieq.supabase.co/rest/v1/";
-
-const SUPABASE_KEY = "sb_publishable_Du6X969aLd1s3oB6kOM6Gw_qT0gdU2z ";
-
-const supabaseClient = supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
-
-console.log("supabase connected");
