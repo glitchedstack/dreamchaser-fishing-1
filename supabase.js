d049614773b2ce65368ef4075b4a7b5ad494c9d6
glitchedstack <jaydimauro6@gmail.com>
2026-07-16 23:34:49 -0400

    Update Supabase URL and key in supabase.js

--- supabase.js
+++ supabase.js
@@ -6,12 +6,12 @@ const supabase = supabaseClient;
 
 let currentUser = null;
 
-const SUPABASE_URL = "https://glitchedstack.github.io/dreamchaser-fishing-1/";
+const SUPABASE_URL = "https://pnuvqgyxiuecdeywswqn.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_NDLHZOKs4FcD7N22EerKgg_FPWZenpI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBudXZxZ3l4aXVlY2RleXdzd3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzE5ODksImV4cCI6MjA5OTgwNzk4OX0.8LmrWskNdFSWfH5f1M-DIS5Fbd4tfIqms-fA5w6tPrU";
 
 
-const db = window.supabase.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
