7c9c5a789d82fde90285f8ac81a30b9fa4b775bd
glitchedstack <jaydimauro6@gmail.com>
2026-07-16 22:58:47 -0400

    Refactor supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,13 +1,17 @@
+// Dreamchaser Fishing App
+
+alert("Dreamchaser script loaded 🎣");
+
+const supabase = supabaseClient;
+
+let currentUser = null;
+
 const SUPABASE_URL = "https://glitchedstack.github.io/dreamchaser-fishing-1/";
 
 const SUPABASE_KEY = "sb_publishable_NDLHZOKs4FcD7N22EerKgg_FPWZenpI";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
