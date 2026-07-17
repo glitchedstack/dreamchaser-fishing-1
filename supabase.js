764b584790f796f8500d9de958c5ee8a1b54692b
glitchedstack <jaydimauro6@gmail.com>
2026-07-16 23:40:17 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,3 @@
-// Dreamchaser Fishing App
-
-alert("Dreamchaser script loaded 🎣");
-
-const supabase = supabaseClient;
-
-let currentUser = null;
-
 const SUPABASE_URL = "https://pnuvqgyxiuecdeywswqn.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBudXZxZ3l4aXVlY2RleXdzd3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzE5ODksImV4cCI6MjA5OTgwNzk4OX0.8LmrWskNdFSWfH5f1M-DIS5Fbd4tfIqms-fA5w6tPrU";
